e0f546611eaffdc32e3f75684f22f60ff3c1de53
ismayilkerimzade <ismayil.programmer@gmail.com>
2023-10-04 08:29:38 +0400

    Finish comboboxes

--- Lesson 1 - Intro ( OperationsOnBooks )/OperationsOnBooks/MainWindow.xaml.cs
+++ Lesson 1 - Intro ( OperationsOnBooks )/OperationsOnBooks/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,42 +22,117 @@ namespace OperationsOnBooks
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+            FillComboboxOfNamesOfCategories();
         }
 
         #region Fields
 
+        //  ConnectionString for connection
         string connectionString = "Data Source=DESKTOP-0V84BDI\\SQLEXPRESS;Initial catalog=Library;Integrated Security=True;Connect Timeout=30;Encrypt=False";
+        
+        //  Connection to database
         SqlConnection? connection;
-        SqlCommand? command;
-        SqlDataReader? reader;
+
+        //  Names of categories for combobox
+        public List<string> NamesOfCategories { get; set; }
+
+        //  Full property Firstnames Of Authors for combobox
+        private List<string> firstnamesOfAuthors;
+        public List<string> FirstnamesOfAuthors { get => firstnamesOfAuthors; set
+            {
+                firstnamesOfAuthors = value;
+                OnPropertyChanged();
+            }
+        }
+
+        //  Selected Name Of Categories for search Firstnames of authors
+        private string? SelectedNameOfCategories = null;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
 
         #endregion
 
+        #region FunctionsOfWindow
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //  Deactive combobox of authors
+            combobox_Authors.IsEnabled = false;
 
-        #region ConnectionToLibrary
+        }
+        private void combobox_Categories_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SelectedNameOfCategories = combobox_Categories.SelectedItem as string;
+            if (SelectedNameOfCategories != null)
+            {
+                combobox_Authors.IsEnabled = true;
+                FillComboboxOfComboboxOfFirstNamesOfAuthorsWithCategoryName(SelectedNameOfCategories);
+            }
+        }
 
-        private void LoadNameOfAuthors(string categoryName)
+
+        #endregion
+
+        #region AssistentFunctions
+        private void OnPropertyChanged([CallerMemberName] string? PrpertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PrpertyName));
+        }
+        private void FillComboboxOfNamesOfCategories()
+        {
+            //  Create connection 
+            connection = new SqlConnection(connectionString);
+
+            try
+            {
+                connection!.Open();
+
+                //  Create command for load names of categories
+                using SqlCommand command = new("SELECT Name FROM Categories\r\nGROUP BY Name", connection);
+
+                //  Create reader for datas
+                using SqlDataReader reader = command.ExecuteReader();
+
+                //  Add datas to combobox
+                NamesOfCategories = new List<string>();
+                while (reader.Read())
+                {
+                    NamesOfCategories.Add((string)reader["Name"]);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+            finally
+            {
+                connection!.Close();
+            }
+        }
+
+        private void FillComboboxOfComboboxOfFirstNamesOfAuthorsWithCategoryName(string categoryName)
         {
             try
             {
-                //  Create connection 
-                connection = new SqlConnection(connectionString);
-                connection.Open();
+                connection!.Open();
 
-                //  Create command
-                string query = "SELECT FirstName FROM Authors\r\nINNER JOIN Books ON Authors.Id = Books.Id_Author\r\nINNER JOIN Categories ON Categories.Id = Books.Id_Category\r\nWHERE Categories.Name = @name";
-                command = new SqlCommand(query, connection);
+                //  Create command for load names of categories
+                using SqlCommand command = new($"SELECT FirstName FROM Authors\r\nINNER JOIN Books ON Books.Id_Author = Authors.Id\r\nINNER JOIN Categories ON Categories.Id = Books.Id_Category\r\nWHERE Categories.Name = '{categoryName}'\r\nGROUP BY FirstName", connection);
 
-                reader = command.ExecuteReader();
+                //  Create reader for datas
+                using SqlDataReader reader = command.ExecuteReader();
 
-                //  Dispose fields
-                
-                command.Dispose();
+                //  Add datas to combobox
+                FirstnamesOfAuthors = new List<string>();
+                while (reader.Read())
+                {
+                    FirstnamesOfAuthors.Add((string)reader["FirstName"]);
+                }
             }
             catch (Exception ex)
             {
@@ -67,5 +145,6 @@ namespace OperationsOnBooks
         }
 
         #endregion
+
     }
 }
